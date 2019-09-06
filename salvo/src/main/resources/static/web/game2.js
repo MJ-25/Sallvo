e98ce849e98ef8050bc7a104011294948cf9b3df
unknown <fabiana.magali@gmail.com>
2019-09-06 20:38:55 -0300

    Task 4.3

--- salvo/src/main/resources/static/web/game2.js
+++ salvo/src/main/resources/static/web/game2.js
@@ -1,8 +1,11 @@
+var horizontal = [1,2,3,4,5,6,7,8,9,10];
+var vertical = ["A","B","C","D","E","F","G","H","I","J"];
+
 function getHeaderSalvo(){
-var headers = "";
+var headers = "<th></th>";
 var i;
-for (i=0; i<=10; i++){
-headers += "<th>" + i + "</th>";
+for (i=0; i<=horizontal.length-1; i++){
+headers += "<th>" + horizontal[i] + "</th>";
 }
 return headers;
 }
@@ -14,13 +17,31 @@ document.getElementById("headSalvo").innerHTML = html;
 }
 
 renderHeadersSalvo();
-/*
+
 var column;
 
 function getBodySalvo(){
 var body = "";
+var headers = "";
+var j;
 var i;
-for (i=0; i<=10; i++){
-body += "<td>" +
-}*/
+for (j=0; j<=horizontal.length-1; j++)
+     {
+        headers += "<td></td>";
+     }
+for (i=0; i<=vertical.length-1; i++)
+{
+//    body += "<tr><td>" + vertical[i] + "</td>";
+  //  body += headers + "</tr>";
+
+    body += "<tr><td>" + vertical[i] + "</td>" + headers + "</tr>";
 }
+ return body;
+}
+
+function renderBodySalvo(){
+var html2 = getBodySalvo();
+document.getElementById("bodySalvo").innerHTML = html2;
+}
+
+renderBodySalvo();

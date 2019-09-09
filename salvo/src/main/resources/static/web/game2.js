38676a322818a6fd983ffb6e17c9c238e6f66322
loreacunia <fabiana.magali@gmail.com>
2019-09-09 20:36:19 -0300

    Tabla de ships

--- salvo/src/main/resources/static/web/game2.js
+++ salvo/src/main/resources/static/web/game2.js
@@ -1,3 +1,7 @@
+$(function() {
+    loadData();
+});
+
 var horizontal = [1,2,3,4,5,6,7,8,9,10];
 var vertical = ["A","B","C","D","E","F","G","H","I","J"];
 
@@ -18,26 +22,24 @@ document.getElementById("headSalvo").innerHTML = html;
 
 renderHeadersSalvo();
 
-var column;
 
 function getBodySalvo(){
 var body = "";
 var headers = "";
 var j;
-var i;
-for (j=0; j<=horizontal.length-1; j++)
-     {
-        headers += "<td></td>";
-     }
+var i=0;
 for (i=0; i<=vertical.length-1; i++)
-{
-//    body += "<tr><td>" + vertical[i] + "</td>";
-  //  body += headers + "</tr>";
-
-    body += "<tr><td>" + vertical[i] + "</td>" + headers + "</tr>";
-}
+    {
+        body += "<tr><td>" + vertical[i] + "</td>";
+         for (j=0; j<=horizontal.length-1; j++)
+              {
+                 body += "<td id='" + vertical[i] + horizontal[j]+ "'></td>";
+              }
+         body += "</tr>";
+    }
  return body;
 }
+
 
 function renderBodySalvo(){
 var html2 = getBodySalvo();
@@ -45,3 +47,32 @@ document.getElementById("bodySalvo").innerHTML = html2;
 }
 
 renderBodySalvo();
+
+function getParameterByName(name) {
+    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+};
+
+function loadData(){
+    $.get('/api/game_view/'+getParameterByName('gp'))
+        .done(function(data) {
+            let playerInfo;
+            if(data.gamePlayers[0].id == getParameterByName('gp'))
+                {playerInfo = [data.gamePlayers[0].player.userName,data.gamePlayers[1].player.userName];}
+            else
+                {playerInfo = [data.gamePlayers[1].player.userName,data.gamePlayers[0].player.userName];}
+
+            $('#playerInfo').text(playerInfo[0] + '(you) vs ' + playerInfo[1]);
+
+            data.ships.forEach(function(ship){
+            console.log(ship);
+                ship.locations.forEach(function(shipLocation){
+                    $('#'+shipLocation).addClass('ship-piece');
+                })
+            });
+        })
+        .fail(function( jqXHR, textStatus ) {
+          alert( "Failed: " + textStatus );
+        });
+};
+

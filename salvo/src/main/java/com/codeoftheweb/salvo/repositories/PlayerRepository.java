d0f4c3bc106016c205e745e4ed3adb4e466b1fdb
loreacunia <fabiana.magali@gmail.com>
2019-09-16 21:38:52 -0300

    Module 6. Task 1.3

--- salvo/src/main/java/com/codeoftheweb/salvo/repositories/PlayerRepository.java
+++ salvo/src/main/java/com/codeoftheweb/salvo/repositories/PlayerRepository.java
@@ -3,9 +3,11 @@ import java.util.List;
 
 import com.codeoftheweb.salvo.models.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface PlayerRepository extends JpaRepository<Player, Long> {
     List<Player> findByUserName(String userName);
+    Player findByEmail(@Param("email") String email);
 }

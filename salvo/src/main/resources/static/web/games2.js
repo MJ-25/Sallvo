f0f99ac9a11b07aa72cd0f91b2eec7bbb4c3d21d
unknown <fabiana.magali@gmail.com>
2019-09-27 21:42:55 -0300

    Log in con form

--- salvo/src/main/resources/static/web/games2.js
+++ salvo/src/main/resources/static/web/games2.js
@@ -1,51 +1,104 @@
-//Obtener Json desde /api/games y colocarlo en e html
-fetch( "/api/games").then(function(response) {
-  if(response.ok){
-  return response.json();
-  }
-}).then(function(json){
-document.getElementById("lista").innerHTML = json.map(listOfGameDates).join("");
-console.log(json);
+$(function () {
 
-
-})
-.catch(function(error) {
-  console.log( "Request failed: " + error.message );
+  $("#logout-btn").hide();
+  $("#login-btn").click(function () {
+    login(event);
+  });
+  $("#logout-btn").click(function () {
+    logout(event);
+  });
 });
 
+
+//Obtener Json desde /api/games y colocarlo en e html
+fetch("/api/games").then(function (response) {
+    if (response.ok) {
+      return response.json();
+    }
+  }).then(function (json) {
+    document.getElementById("lista").innerHTML = json.map(listOfGameDates).join("");
+    console.log(json);
+
+
+  })
+  .catch(function (error) {
+    console.log("Request failed: " + error.message);
+  });
+
 //Crear la lista para poner en el html
-function listOfGameDates(game){
-return "<li class='collection-item deep-orange darken-3'> Horario: " + game.created + "   Jugadores: "+ game.gamePlayers.map(emails) + "</li>"
+function listOfGameDates(game) {
+  return "<li class='collection-item deep-orange darken-3'> Horario: " + game.created + "   Jugadores: " + game.gamePlayers.map(emails) + "</li>"
 }
 
-function emails (e){
-var mails = " " + e.player.email;
-return mails;
+function emails(e) {
+  var mails = " " + e.player.email;
+  return mails;
 }
 
 //Obtiene Json desde /api/leaderBoard y lo pone en el html
-fetch( "/api/leaderBoard").then(function(response) {
-  if(response.ok){
-  return response.json();
-  }
-}).then(function(json){
-document.getElementById("bodyLeaderBoard").innerHTML = json.map(tableBody).join("");
-console.log(json);
+fetch("/api/leaderBoard").then(function (response) {
+    if (response.ok) {
+      return response.json();
+    }
+  }).then(function (json) {
+    document.getElementById("bodyLeaderBoard").innerHTML = json.map(tableBody).join("");
+    console.log(json);
 
 
-})
-.catch(function(error) {
-  console.log( "Request failed: " + error.message );
-});
+  })
+  .catch(function (error) {
+    console.log("Request failed: " + error.message);
+  });
 
 
 //Crea el contenido que va adentro de la tabla
-function tableBody(e){
-return "<tr><td>"+ e.email +
-        "</td><td>" + e.score.total +
-        "</td><td>" + e.score.won +
-        "</td><td>" + e.score.lost +
-        "</td><td>" + e.score.tied +
-        "</td><td>" + e.score.gamesPlayed +
-        "</td></tr>";
+function tableBody(e) {
+  return "<tr><td>" + e.email +
+    "</td><td>" + e.score.total +
+    "</td><td>" + e.score.won +
+    "</td><td>" + e.score.lost +
+    "</td><td>" + e.score.tied +
+    "</td><td>" + e.score.gamesPlayed +
+    "</td></tr>";
+}
+
+function login(evt) {
+  evt.preventDefault();
+  var form = evt.target.form;
+  console.log(form)
+  $.post("/api/login", {
+      name: form["name"].value,
+      password: form["password"].value
+    })
+    .done(function (data) {
+      console.log("successful login!!"),
+        showLogin(false);
+    })
+     .fail(function( jqXHR, textStatus ) {
+              alert( "Failed: " + textStatus );
+            });
+    };
+
+
+function logout(evt) {
+  evt.preventDefault();
+  $.post("/api/logout")
+  .done(function (data) {
+        console.log("successful logout!!"),
+          showLogin(true);
+      })
+   .fail(function( jqXHR, textStatus ) {
+                       alert( "Failed: " + textStatus );
+                     });
+}
+
+function showLogin(show) {
+  if (show) {
+    $("#login-form").show();
+    //$("#user-panel").hide();
+  } else {
+    $("#logout-btn").show();
+    $("#login-form").hide();
+    //$("#user-panel").show();
+  }
 }
